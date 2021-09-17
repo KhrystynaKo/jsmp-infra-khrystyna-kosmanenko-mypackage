8d38199a251a569e302902a2c5a565b1bd9ea3a9
Khrystyna Kosmanenko <Khrystyna_Kosmanenko@epam.com>
2021-09-17 14:42:01 +0300

    fixes

--- src/tests/createSectionList.test.ts
+++ src/tests/createSectionList.test.ts
@@ -1,25 +1,25 @@
-import { createSectionList } from '../createSectionList';
+import { createSectionList } from "../createSectionList";
 
-describe('createSectionList', () => {
-    const input = ['Anna', 'Loren', 'Kate', 'Anabel', 'Leo']
+describe("createSectionList", () => {
+  const input = ["Anna", "Loren", "Katy", "Anabel", "Leo"];
 
-    const output = [
-        {
-            name: 'A',
-            data: ['Anabel', 'Anna'],
-        },
-        {
-            name: 'K',
-            data: ['Kary'],
-        },
-        {
-            name: 'L',
-            data: ['Leo', 'Loren'],
-        },
-    ];
+  const output = [
+    {
+      name: "A",
+      data: ["Anabel", "Anna"],
+    },
+    {
+      name: "K",
+      data: ["Katy"],
+    },
+    {
+      name: "L",
+      data: ["Leo", "Loren"],
+    },
+  ];
 
-    it('should return section list', () => {
-        const result = createSectionList(input);
-        expect(result).toEqual(output);
-    });
+  it("should return section list", () => {
+    const result = createSectionList(input);
+    expect(result).toEqual(output);
+  });
 });
